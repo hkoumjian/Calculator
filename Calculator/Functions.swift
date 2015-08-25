81eda8950e3b3049e7d7e9147c496778d5589781
hkoumjian <hkoumjian@umass.edu>
2015-08-24 22:44:37 -0400

    First commit

---
+++ Calculator/Functions.swift
@@ -0,0 +1,13 @@
+//
+//  Functions.swift
+//  Calculator
+//
+//  Created by Harris Koumjian on 8/21/15.
+//  Copyright (c) 2015 Harris Koumjian. All rights reserved.
+//
+
+import Foundation
+
+func add(valueOne: Double, valueTwo: Double) ->Double{
+    
+}
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,8 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+extension String{
+    var doubleValue: Double{
+        if let number = NSNumberFormatter().numberFromString(self) {
+            return number.doubleValue
+        }
+        return 0
+    }
+}
 
+class ViewController: UIViewController {
+    
+    var isTypingNumber = false
+    var firstNumber:Double? = 0
+    var secondNumber:Double? = 0
+    var operation = ""
+    
+    
+    @IBOutlet weak var calculatorDisplay: UILabel!
+    @IBAction func numberTapped(sender: AnyObject) {
+        var number = sender.currentTitle
+        
+        if isTypingNumber {
+            calculatorDisplay.text = calculatorDisplay.text! + number!!
+        } else{
+            calculatorDisplay.text = number
+            isTypingNumber = true
+        }
+    }
+    @IBAction func calculationTapped(sender: AnyObject) {
+        isTypingNumber = false
+        firstNumber = calculatorDisplay.text!.doubleValue
+        operation = sender.currentTitle!!
+    }
+    @IBAction func equalsTapped(sender: AnyObject) {
+        isTypingNumber = false
+        var result = 0.0
+        secondNumber = calculatorDisplay.text!.doubleValue
+        
+        if operation == "+"{
+            result = firstNumber! + secondNumber!
+        } else if operation == "-"{
+            result = firstNumber! - secondNumber!
+        } else if operation == "*"{
+            result = firstNumber! * secondNumber!
+        }
+        else if operation == "/"{
+            result = firstNumber! / secondNumber!
+        }
+        
+        calculatorDisplay.text = "\(result)"
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
